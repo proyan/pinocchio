48bd5ea7d51991a4e1e46c6f2162e5c7b84f0f82
Justin Carpentier <justin.carpentier@inria.fr>
2020-03-02 14:04:04 +0100

    core: rename ContactInfoTpl into RigidContactModelTpl

--- src/algorithm/contact-cholesky.hxx
+++ src/algorithm/contact-cholesky.hxx
@@ -21,9 +21,9 @@ namespace pinocchio
     void
     ContactCholeskyDecompositionTpl<Scalar,Options>::
     allocate(const ModelTpl<S1,O1,JointCollectionTpl> & model,
-             const std::vector<ContactInfoTpl<S1,O1>,Allocator> & contact_infos)
+             const std::vector<RigidContactModelTpl<S1,O1>,Allocator> & contact_infos)
     {
-      typedef ContactInfoTpl<S1,O1> ContactInfo;
+      typedef RigidContactModelTpl<S1,O1> ContactInfo;
       typedef std::vector<ContactInfo,Allocator> ContactInfoVector;
       
       nv = model.nv;
@@ -189,10 +189,10 @@ namespace pinocchio
     void ContactCholeskyDecompositionTpl<Scalar,Options>::
     compute(const ModelTpl<S1,O1,JointCollectionTpl> & model,
             DataTpl<S1,O1,JointCollectionTpl> & data,
-            const std::vector<ContactInfoTpl<S1,O1>,Allocator> & contact_infos,
+            const std::vector<RigidContactModelTpl<S1,O1>,Allocator> & contact_infos,
             const S1 mu)
     {
-      typedef ContactInfoTpl<S1,O1> ContactInfo;
+      typedef RigidContactModelTpl<S1,O1> ContactInfo;
       typedef MotionTpl<Scalar,Options> Motion;
       assert(model.check(data) && "data is not consistent with model.");
       PINOCCHIO_CHECK_INPUT_ARGUMENT((Eigen::DenseIndex)contact_infos.size() == num_contacts,
